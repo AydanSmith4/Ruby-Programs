470ea56701161644a7b5e61860007e0ae18c4b73
AydanSmith4 <51413372+AydanSmith4@users.noreply.github.com>
2019-06-05 15:57:06 -0500

    Update tweet.rb

--- tweet.rb
+++ tweet.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 account = Twitter::REST::Client.new do |config|
-    config.consumer_key = "Consumer Key"
-    config.consumer_secret = "Consumer Secret"
-    config.access_token = "Access Token"
-    config.access_token_secret = "Access Token Secret"
+    config.consumer_key = "Consumer Key" #put in your personal consumer key
+    config.consumer_secret = "Consumer Secret" #put in your personal consumer secret key
+    config.access_token = "Access Token" #put in your personal access token
+    config.access_token_secret = "Access Token Secret" #put in your personal access token secret
 end
 account.update("My First Tweet Using the Ruby Program!")
